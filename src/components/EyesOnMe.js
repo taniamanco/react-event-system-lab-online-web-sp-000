e698b240069c8d7abf5063e548d63c6ea01675f6
Tania Manco <belalov.tania16@gmail.com>
2020-01-13 21:17:35 +0000

    Automatically backed up by Learn

---
+++ src/components/EyesOnMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  onFocus = () => console.log('Good!');
+  onBlur = () => console.log('Hey! Eyes on me!')
+
+  render() {
+    return (<button onFocus={this.onFocus} onBlur={this.onBlur}>
+        Eyes on me, please!
+    </button>
+    )
+  }
+}
+
+export default EyesOnMe;
